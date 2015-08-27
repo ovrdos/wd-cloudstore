677ddb1b3c5c57d5782c1960afe72c0be63ffd67
Kamal <ovrdos@users.noreply.github.com>
2015-08-27 10:03:16 -0700

    Set first random

--- disq_ads.js
+++ disq_ads.js
@@ -2,7 +2,7 @@ var disqus_shortname = 'worldstar'; disqus_identifier = '85324'; var disqus_titl
 (function() { var dsq = document.createElement('script'); 
 dsq.type = 'text/javascript'; dsq.async = true; dsq.id = 'disqus_script'; 
 dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js'; 
-(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq); })(); getVids(first_video_paid); 
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq); })(); getVids(); 
 setTimeout(function() { $('img#newlogo').hide(); 
 $('div.ctrl').fadeIn(1000); },8000); 
 $('.ctrl').off('click');

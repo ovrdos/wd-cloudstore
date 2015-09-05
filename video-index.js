e0a662a1170c9fd04d6af665f91c448e3d5a3478
Kamal Hakim <kamalhakim@livenation.com>
2015-09-04 21:49:03 -0700

    Video update.

--- video-index.js
+++ video-index.js
@@ -1,8 +1,4 @@
 var AddVideo = function() { this.addVariable = function(k,v) { if (v) { vids.push(v); keys.push(k); } }}; var getNewVideos = function() { so = new AddVideo();
-	so.addVariable("85822","http://hw-videos.worldstarhiphop.com/u/vid/2015/09/YOet5AHouDu0.mp4");
-	so.addVariable("85736","http://www.youtube.com/v/1YDf3zBWOCY");
-	so.addVariable("85646","http://www.youtube.com/v/oOW1mQ9wOW4");
-	so.addVariable("85904","http://hw-videos.worldstarhiphop.com/u/vid/2015/09/ceDLp4qr41Lu.mp4");
+	so.addVariable("85732","http://hw-videos.worldstarhiphop.com/u/vid/2015/08/IAIStjwxXxqx.mp4");
 	so.addVariable("85556","http://www.youtube.com/v/ckNeQLigCkk");
 }
-	so.addVariable("85821","http://hw-videos.worldstarhiphop.com/u/vid/2015/09/rt2t9LUGx9C2.mp4");
